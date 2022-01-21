126512467754dd789e510a8a0c0b4dffa42422dd
Mr-Mahlakoana <Mahlakoana.Lehlogonolo@gmail.com>
2022-01-21 12:33:32 +0200

    fixed api issue

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': '5618cb4830mshb06a906356222d7p138a5cjsn3db13ad0a00b',
+      'x-rapidapi-key': process.env.API_KEY,
     },
   });
     
